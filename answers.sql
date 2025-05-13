d4f8d978681481dd2276c6bc6fa99a2aea51485a
Reabetswe <reamothiba@icloud.com>
2025-05-13 19:28:00 +0200

    Add files via upload

--- answers.sql
+++ answers.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE salesDB;
-DROP DATABASE demo;
+DROP INDEX IdxPhone ON customers;
+#This query removes the index named IdxPhone from the customers table. If the index doesn't exist, MySQL will return an error.#
+
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+#This query creates a new MySQL user bob who can only connect from the localhost (your local machine). The password for the user is 'S$cu3r3!'.U#
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+#This query changes the password for the user bob to 'P$55!23'.#
+
+
+
+
+
